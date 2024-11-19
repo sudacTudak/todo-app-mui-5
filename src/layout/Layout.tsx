a4e6282b078fe94d0083c0486aa8e79af0cf07ff
sudac_tudak <30slava09@mail.ru>
2024-11-19 15:02:51 +0300

    changed Layout markup and styles

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,53 +1,42 @@
-import { Box, Container, Typography } from '@mui/material';
+import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
 import { LayoutProps } from './Layout.props';
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <Box
       sx={{
-        backgroundColor: '#EEEEEE',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center'
+        height: '100%'
       }}
     >
-      <Container maxWidth="md">
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            rowGap: '30px'
-          }}
-        >
-          <Typography
-            component="h1"
-            variant="h1"
-            noWrap
-            sx={{
-              alignSelf: 'center',
-              padding: '0 15px',
-              borderBottomWidth: '3px',
-              borderBottomStyle: 'solid',
-              borderBottomColor: 'primary.light',
-              fontWeight: 700,
-              color: 'primary.light'
-            }}
-          >
-            TodoApp
+      <AppBar sx={{ bgcolor: 'background.default' }}>
+        <Toolbar>
+          <Typography variant="h5" color="primary.light" fontWeight="700">
+            WhatTodo
           </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+          bgcolor: 'primary.light'
+        }}
+      >
+        <Container maxWidth="md">
           <Box
             sx={{
-              height: '50vh',
-              padding: '10px',
+              p: '10px',
               borderRadius: '10px',
-              bgcolor: 'background.default'
+              bgcolor: 'background.default',
+              height: '50vh'
             }}
           >
             {children}
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      </Box>
     </Box>
   );
 };
